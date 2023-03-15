cb1cdda87c1b91e80c2fc5ec26e0372aa9bbd778
Aleksandr <a.teslin90@gmail.com>
2023-03-15 14:53:14 +0300

    init: start modules 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <h1
+      id="dinasur_name"
+      className="dinasaur_name"
+      aria-describedby="dinosaur_name_info"
+    >
+      Hello World!
+    </h1>
+  );
+}
+
+export default App;
