f4ade7be201d8e4ac47ddefe339a3344ca811d93
Aleksandr <a.teslin90@gmail.com>
2023-04-06 17:49:44 +0300

    feat: add modal open on click

--- src/components/cardItem/CardItem.tsx
+++ src/components/cardItem/CardItem.tsx
@@ -1,13 +1,22 @@
+import React, { SetStateAction } from 'react';
 import styles from './CardItem.module.scss';
 
 interface CardItemProps {
   name: string;
   image: string;
+  id: number;
+  setModalId: React.Dispatch<SetStateAction<string>>;
 }
 
-const CardItem = ({ image, name }: CardItemProps) => {
+const CardItem = ({ image, name, id, setModalId }: CardItemProps) => {
   return (
-    <div className={styles.wrapper}>
+    <div
+      role="presentation"
+      className={styles.wrapper}
+      onClick={() => {
+        setModalId(`${id}`);
+      }}
+    >
       <img className={styles.img} src={image} alt={name} />
       <div className={styles.text}>
         <b>Character Name: </b>
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import CardItem from '../cardItem/CardItem';
 import styles from './CardList.module.scss';
 import { RickMortyCharaterDataResult } from '../../interfaces/interfaces';
+import Modal from '../modal/Modal';
 
 interface CardListProps {
   results: RickMortyCharaterDataResult[];
 }
 
 const CardList = ({ results }: CardListProps) => {
+  const [modalId, setModalId] = useState('');
   return (
-    <div className={styles['card-list']}>
-      {results.map(({ name, image, id }: RickMortyCharaterDataResult) => {
-        return <CardItem key={id} name={name} image={image} />;
-      })}
-    </div>
+    <>
+      {modalId && <Modal id={modalId} setModalId={setModalId} />}
+      <div
+        className={
+          modalId
+            ? `${styles['card-list']} ${styles.modal}`
+            : `${styles['card-list']}`
+        }
+      >
+        {results.map(({ name, image, id }: RickMortyCharaterDataResult) => {
+          return (
+            <CardItem
+              key={id}
+              name={name}
+              image={image}
+              id={id}
+              setModalId={setModalId}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
